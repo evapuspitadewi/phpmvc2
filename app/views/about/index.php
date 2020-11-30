bc3a020a587ac7a48d4aa196cc278f360d48c191
evapuspitadewi <evapuspitadewi202@gmail.com>
2020-11-29 19:30:47 -0800

    menambahkan file index

---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profile.jpg" alt="Sandhika Galih" width="200" class="rounded-circle shadow">
+    
